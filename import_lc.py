7f88c2d5ddd425cfd10e463fce6129b03bf51151
Sabyasachi <yoboy907@gmail.com>
2023-03-06 22:54:25 +0530

    Done.

--- import_lc.py
+++ import_lc.py
@@ -1,14 +1,11 @@
 import os
-import sys
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def get_links(driver):
@@ -34,62 +31,81 @@ def get_links(driver):
 	ff.click()
 	time.sleep(1)
 
-
-	# getting the list of problems
-
-	# this is the list of div of rows
-	list_of_problems = driver.find_elements(By.XPATH, ".//div[@role = 'rowgroup']//div[@role = 'row']")
-	
 	list_of_links = []
 	list_of_names = []
 
-	for i in list_of_problems:
+	def find(driver):
+		# getting the navigation element
+		return driver.find_elements(By.XPATH, ".//nav[@role = 'navigation']//button[@aria-label='next']")[-1]
 
-		# this gets the div of the row
-		innerDiv = i.find_elements(By.XPATH, ".//div[@role = 'cell']")[1]
-		
-		name = innerDiv.text # this gets the name of the problem
-		link = innerDiv.find_element(By.TAG_NAME,'a').get_attribute("href") # gets the link of the problem
-		
-		list_of_links.append(link)
-		list_of_names.append(name)
-	
+	page = 1
+
+	while(True):
+		# getting the list of problems
+		print("Page: " + str(page))
+
+		if page != 1:
+			navigationButton = WebDriverWait(driver, 20).until(find)
+			try: 
+				navigationButton.click()
+			except Exception:
+				break
+
+		# this is the list of div of rows
+		list_of_problems = driver.find_elements(By.XPATH, ".//div[@role = 'rowgroup']//div[@role = 'row']")
+
+		for i in list_of_problems:
+
+			# this gets the div of the row
+			innerDiv = i.find_elements(By.XPATH, ".//div[@role = 'cell']")[1]
+			
+			name = innerDiv.text # this gets the name of the problem
+			link = innerDiv.find_element(By.TAG_NAME,'a').get_attribute("href") # gets the link of the problem
+			
+			list_of_links.append(link)
+			list_of_names.append(name)
+
+		time.sleep(5)
+
+		page = page + 1
+
 	totalnumber = len(list_of_problems)
 	print ("Total number of problems: " + str(totalnumber))
 
 	print(list_of_links)
 	print(list_of_names)
 
-	return list_of_links[1:], list_of_names
+	list_of_links = list_of_links[1:]
+	list_of_names = list_of_names[1:]
 
-
-def print_links(driver):
-	list_of_links = get_links(driver)
-	for a in list_of_links:
-		print (a.text)
+	return list_of_links, list_of_names
 
 
 def main(args):
+	# setting up the driver
 	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 	driver.maximize_window()
 
-	print ("Opening LeetCode...")
+	# going to the leetcode page
 	driver.get("https://leetcode.com/")
 
-	print ("Executing login...")
+	# login
 	link = driver.find_element("link text", "Sign in")
 	link.click()
 	try:
-		elem = WebDriverWait(driver, 30).until(
-		EC.presence_of_element_located((By.ID, "id_login")) #This is a dummy element
+		# waiting till the login form is loaded
+		WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "id_login"))
 	)
 	finally:
+		# username field
 		form_textfield = driver.find_element(By.ID, 'id_login')
 		form_textfield.send_keys(args.email)
 
+		# password field
 		form_textfield2 = driver.find_element(By.ID, 'id_password')
 		form_textfield2.send_keys(args.password)
 
+		# sign in button	
 		nextButton = driver.find_element(By.ID, 'signin_btn')
 		driver.execute_script("arguments[0].click();", nextButton)
 
@@ -99,24 +115,26 @@ def main(args):
 		finally:
 			print ("Login completed...")
 			i = 0
-
 			time.sleep(5)
 
+			# going to problem page
 			driver.get("https://leetcode.com/problemset/all/")
 
+			# getting the list of links and names of them
 			links_to_problems, names = get_links(driver)
 
+			# making the folders and files
 			while (i < len(links_to_problems)):
 
 				problemname = names[i].split(".")[-1].replace(".", "").strip() # getting the name of the problem
 
 				folder = str(args.path) + "/" + problemname + "/"
 
-				if os.path.exists(folder):
-					i = i + 1
-					continue
+				# if os.path.exists(folder):
+				# 	i = i + 1
+				# 	continue
 
-				if not os.path.exists(folder):
+				if True:
 					os.makedirs(folder)
 					driver.get(links_to_problems[i])
 					time.sleep(2)
@@ -124,7 +142,7 @@ def main(args):
 					# Grep problem's specifications
 					problem = driver.find_element(By.CLASS_NAME, "_1l1MA") # this gets the div with the div of the text of the problem
 
-					question = str(problem.text.encode('ascii', 'ignore').decode('ascii')) # this gets the question of the problem
+					question = problem.get_attribute("innerHTML") # this gets the question of the problem
 
 					# getting submission button
 					nextButton = driver.find_element("link text", "Submissions")
@@ -139,11 +157,11 @@ def main(args):
 					# getting the first accepted submission code 
 					code_page = driver.find_element(By.TAG_NAME, "code")
 
+					# grepping the language of the code
 					lang = code_page.get_attribute("class")
-					print(lang)
-
 					time.sleep(5)
 
+					# setting the extension of the file
 					if "language-java" in lang:
 						ext = ".java"
 					elif "language-python" in lang:
@@ -153,19 +171,23 @@ def main(args):
 					elif "language-c" in lang:
 						ext = ".c"
 
+					# making the file
 					f = open(folder + problemname.replace(" ", "-").lower() + ext,  'w+')
 
+					# writing the code to the file
 					f.write(code_page.text)
 					f.flush()
 					f.close()
 
+					# making the readme file
 					f = open(folder + f"{problemname}.md", "w+")
-					f.write(f"# {problemname}" + "\n## From: " + driver.current_url + "\n\n")
+					f.write(f"# {problemname}" + f"\n### [LeetCode Link]({driver.current_url})\n")
 					f.write(question)
 					f.flush()
 					f.close
 
 				i = i + 1
+
 			driver.close()
 
 def parse_args():
