9cbdce2efe87b554988fb5b433aee62c3c4828ac
Sabyasachi <yoboy907@gmail.com>
2023-05-01 11:42:40 +0530

    Minor Fixes

--- GitCode.py
+++ GitCode.py
@@ -1,49 +1,40 @@
 # installing dependencies
-# import os
-# os.system("pip install selenium")
-# os.system("pip install webdriver_manager")
 import os
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.common.by import By
-from selenium import webdriver
 import time
 
 def get_links(driver):
     # getting the solved problems
 
     # Status button
-    while(True):
+    while (True):
         try:
-            ff = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Status')]")))
+            ff = WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+                (By.XPATH, "//*[contains(text(), 'Status')]")))
             ff.click()
             break
-        except:
+        except Exception as e:
             driver.refresh()
             time.sleep(5)
-        
+
     time.sleep(1)
 
     # Solved button
-    
-    while(True):
+
+    while (True):
         try:
-            ff = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Solved')]")))
+            ff = WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+                (By.XPATH, "//*[contains(text(), 'Solved')]")))
             ff.click()
             break
-        except:
+        except Exception as e:
             driver.refresh()
             time.sleep(5)
-        
+
     time.sleep(1)
 
     # need to wait while the page loads, this checks if the reset button is loaded
-    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Reset')]")))
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+        (By.XPATH, "//*[contains(text(), 'Reset')]")))
     time.sleep(10)
 
     list_of_links = []
@@ -54,51 +45,57 @@ def get_links(driver):
 
     while (True):
 
-        if(breakOut):
+        if (breakOut):
             break
         # getting the list of problems
         print("Page: " + str(page))
 
         if page != 1:
-            while(True):
+            while (True):
                 try:
-                    nextButton = driver.find_elements(By.XPATH, ".//nav[@role = 'navigation']//button[@aria-label='next']")[-1]
+                    nextButton = driver.find_elements(
+                        By.XPATH, ".//nav[@role = 'navigation']//button[@aria-label='next']")[-1]
                     time.sleep(1)
                     try:
                         nextButton.click()
-                    except:
+                    except Exception as e:
                         breakOut = True
                         break
                 except Exception as e:
                     print(e)
                     driver.refresh()
                     time.sleep(3)
-                    
 
-        ind = 0;
+        ind = 0
 
-        while(ind < len(driver.find_elements(By.XPATH, ".//div[@role = 'rowgroup']//div[@role = 'row']"))):
+        while (ind < len(driver.find_elements(By.XPATH, ".//div[@role = 'rowgroup']//div[@role = 'row']"))):
 
             # this gets the div of the row
-            while(True):
+            while (True):
                 try:
-                    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, ".//div[@role = 'rowgroup']//div[@role = 'row']")))
-                    innerDiv = driver.find_elements(By.XPATH, ".//div[@role = 'rowgroup']//div[@role = 'row']")[ind]
-                    WebDriverWait(innerDiv, 30).until(EC.presence_of_element_located((By.XPATH, ".//div[@role = 'cell']")))
-                    innerDiv = innerDiv.find_elements(By.XPATH, ".//div[@role = 'cell']")[1]
+                    WebDriverWait(driver, 30).until(EC.presence_of_element_located(
+                        (By.XPATH, ".//div[@role = 'rowgroup']//div[@role = 'row']")))
+                    innerDiv = driver.find_elements(
+                        By.XPATH, ".//div[@role = 'rowgroup']//div[@role = 'row']")[ind]
+                    WebDriverWait(innerDiv, 30).until(
+                        EC.presence_of_element_located((By.XPATH, ".//div[@role = 'cell']")))
+                    innerDiv = innerDiv.find_elements(
+                        By.XPATH, ".//div[@role = 'cell']")[1]
                     break
-                except:
+                except Exception as e:
                     driver.refresh()
                     time.sleep(5)
 
             name = innerDiv.text  # this gets the name of the problem
-                
-            while(True):
+
+            while (True):
                 try:
-                    link = WebDriverWait(innerDiv, 30).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
-                    link = link.get_attribute("href")  # gets the link of the problem
+                    link = WebDriverWait(innerDiv, 30).until(
+                        EC.presence_of_element_located((By.TAG_NAME, 'a')))
+                    # gets the link of the problem
+                    link = link.get_attribute("href")
                     break
-                except:
+                except Exception as e:
                     driver.refresh()
                     time.sleep(5)
 
@@ -122,17 +119,20 @@ def get_links(driver):
 
     return list_of_links, list_of_names
 
+
 def acceptAlert(alert):
     try:
         alert.accept()
-    except:
+    except Exception as e:
         pass
+
 
 def initialiseDriver(option):
     if option == 1:
         return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     elif option == 2:
         return webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
 
 def main(path='.'):
     driver, selected = "", False
@@ -174,34 +174,39 @@ def main(path='.'):
     driver.get("https://leetcode.com/")
 
     # printing the nessary info for the user not to login
-    driver.execute_script("window.alert('Greetings from GitCode! Please do not interact with the browser now.')")
+    driver.execute_script(
+        "window.alert('Greetings from GitCode! Please do not interact with the browser now.')")
     time.sleep(5)
     acceptAlert(alert=alert)
 
     # login
-    link = WebDriverWait(driver, 30).until(EC.presence_of_element_located(("link text", "Sign in")))
+    link = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located(("link text", "Sign in")))
     link.click()
     try:
         # waiting till the login form is loaded
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "id_login")))
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.ID, "id_login")))
     finally:
         try:
-            driver.execute_script("window.alert('Now please login to your account.')")
+            driver.execute_script(
+                "window.alert('Now please login to your account.')")
             time.sleep(5)
             acceptAlert(alert=alert)
 
             # waiting till the login is completed
             sleepTime = 0
-            sleepMax = 180 # 3 mins
-            while(len(driver.find_elements(By.XPATH, ".//div[@id = 'navbar-right-container']//div")) <= 1):
+            sleepMax = 180  # 3 mins
+            while (len(driver.find_elements(By.XPATH, ".//div[@id = 'navbar-right-container']//div")) <= 1):
                 time.sleep(1)
                 sleepMax = sleepMax + 1
                 if sleepTime > sleepMax:
-                    sleepTime = 0;
+                    sleepTime = 0
                     driver.refresh()
         finally:
             print("Login completed...")
-            driver.execute_script("window.alert('Now sit back and watch GitCode do the work for you!')")
+            driver.execute_script(
+                "window.alert('Now sit back and watch GitCode do the work for you!')")
             time.sleep(5)
             acceptAlert(alert=alert)
 
@@ -221,7 +226,7 @@ def main(path='.'):
                 problemname = names[i].split(".")[-1].replace(".", "").strip()
 
                 folder = str(path) + "/" + problemname + "/"
-                
+
                 if os.path.exists(folder):
                     i = i + 1
                     continue
@@ -233,46 +238,49 @@ def main(path='.'):
                 while True:
                     try:
                         # this gets the div with the div of the text of the problem
-                        problem = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "_1l1MA")))
+                        problem = WebDriverWait(driver, 30).until(
+                            EC.presence_of_element_located((By.CLASS_NAME, "_1l1MA")))
 
                         # this gets the question of the problem
-                        question = problem.get_attribute("innerHTML").encode('utf-8')
+                        question = problem.get_attribute(
+                            "innerHTML").encode('utf-8')
                         question = question.decode('utf-8')
 
                         # getting submission button
-                        nextButton = WebDriverWait(driver, 30).until(EC.presence_of_element_located(("link text", "Submissions")))
+                        nextButton = WebDriverWait(driver, 30).until(
+                            EC.presence_of_element_located(("link text", "Submissions")))
                         nextButton.click()
                         break
-                    except:
+                    except Exception as e:
                         driver.refresh()
 
                 # getting the accepted button
-                while(True):
+                while (True):
                     try:
-                        nextButton = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Accepted')]")))
+                        nextButton = WebDriverWait(driver, 30).until(
+                            EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Accepted')]")))
                         nextButton.click()
                         break
-                    except:
+                    except Exception as e:
                         driver.refresh()
 
-                
-
                 # getting the first accepted submission code
-                while(True):
+                while (True):
                     try:
-                        code_page = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "code")))
+                        code_page = WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.TAG_NAME, "code")))
                         code = code_page.text
                         break
-                    except:
+                    except Exception as e:
                         driver.refresh()
                         time.sleep(2)
 
                 # grepping the language of the code
-                while(True):
+                while (True):
                     try:
                         lang = code_page.get_attribute("class")
                         break
-                    except:
+                    except Exception as e:
                         continue
 
                 time.sleep(2)
@@ -288,9 +296,9 @@ def main(path='.'):
                 elif "language-c" in lang:
                     ext = ".c"
 
-
                 # making the file
-                f = open(folder + problemname.replace(" ","-").lower() + ext,  'w+', encoding="utf-8")
+                f = open(folder + problemname.replace(" ",
+                         "-").lower() + ext,  'w+', encoding="utf-8")
 
                 # writing the code to the file
                 f.write(code)
@@ -307,40 +315,50 @@ def main(path='.'):
                 os.system(f"git add {path}/")
 
                 # getting the runtime, runtime beats, memory, and memory beats
-                while(True):
+                while (True):
                     try:
                         # this is just the runtime text element
-                        runtimeElement = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Runtime')]")))
+                        runtimeElement = WebDriverWait(driver, 30).until(
+                            EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Runtime')]")))
                         # this is the parent div of the runtime text element
-                        runtimeParent = runtimeElement.find_element(By.XPATH, "..")
+                        runtimeParent = runtimeElement.find_element(
+                            By.XPATH, "..")
                         # this is the parent div of the parent div of the whole runtime element
-                        runtimeClass = runtimeParent.find_element(By.XPATH, "..")
+                        runtimeClass = runtimeParent.find_element(
+                            By.XPATH, "..")
                         # this is the beats % for the runtime
-                        runtimeBeats = runtimeClass.find_element(By.XPATH, ".//*[contains(text(), 'Beats')]")
+                        runtimeBeats = runtimeClass.find_element(
+                            By.XPATH, ".//*[contains(text(), 'Beats')]")
                         # this is the parent div of the beats% for the
-                        runtimeBeatsParent = runtimeBeats.find_element(By.XPATH, "..")
+                        runtimeBeatsParent = runtimeBeats.find_element(
+                            By.XPATH, "..")
                         # this is the text of the beats% for the runtime
                         runtimeBeats = runtimeBeatsParent.text.split("\n")[-1]
                         # this is the runtime used
-                        runtime = runtimeParent.text.split("\n")[-1].replace(" ", "")
+                        runtime = runtimeParent.text.split(
+                            "\n")[-1].replace(" ", "")
 
                         # print(runtime, runtimeBeats)
 
-
                         # this is just the memory text element
-                        memoryElement = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Memory')]")))
+                        memoryElement = WebDriverWait(driver, 30).until(
+                            EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Memory')]")))
                         # this is the parent div of the memory text element
-                        memoryParent = memoryElement.find_element(By.XPATH, "..")
+                        memoryParent = memoryElement.find_element(
+                            By.XPATH, "..")
                         # this is the parent div of the parent div of the whole memory element
                         memoryClass = memoryParent.find_element(By.XPATH, "..")
                         # this is the beats % for the memory
-                        memoryBeats = memoryClass.find_element(By.XPATH, ".//*[contains(text(), 'Beats')]")
+                        memoryBeats = memoryClass.find_element(
+                            By.XPATH, ".//*[contains(text(), 'Beats')]")
                         # this is the parent div of the beats% for the
-                        memoryBeatsParent = memoryBeats.find_element(By.XPATH, "..")
+                        memoryBeatsParent = memoryBeats.find_element(
+                            By.XPATH, "..")
                         # this is the text of the beats% for the memory
                         memoryBeats = memoryBeatsParent.text.split("\n")[-1]
                         # this is the memory used
-                        memory = memoryParent.text.split("\n")[-1].replace(" ", "")
+                        memory = memoryParent.text.split(
+                            "\n")[-1].replace(" ", "")
 
                         # print(memory, memoryBeats)
 
@@ -353,20 +371,21 @@ def main(path='.'):
 
                 commitMessage = f"Time: {runtime} ({runtimeBeats}) | Memory: {memory} ({memoryBeats}) - GitCode"
 
-                 # now we need to commit the changes with proper commit message
+                # now we need to commit the changes with proper commit message
                 for file in os.listdir(folder):
-                    commitFilname = folder.replace(" ", "\ ") + file.replace(" ","\ ") 
+                    commitFilname = folder.replace(
+                        " ", "\ ") + file.replace(" ", "\ ")
                     addMessage = f"git add {commitFilname}"
                     print(addMessage)
                     os.system(addMessage)
-                    
 
                 commitMessage = f"git commit -m '{commitMessage}'"
                 os.system(commitMessage)
 
                 i = i + 1
 
-            driver.execute_script("window.alert('You solved solutions have been exported to your GitHub repository! Thanks for using GitCode!')")
+            driver.execute_script(
+                "window.alert('You solved solutions have been exported to your GitHub repository! Thanks for using GitCode!')")
             time.sleep(5)
             acceptAlert(alert=alert)
 
@@ -374,8 +393,15 @@ def main(path='.'):
 
 
 if __name__ == "__main__":
-    os.system("pip install -r requirements.txt")
-    os.system("git pull")
-    os.system("clear")
-    main(path = input("Enter the path to save the files: "))
+    os.system("clear && pip install -r requirements.txt && git pull && clear")
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.alert import Alert
+    from selenium.webdriver.chrome.service import Service
+    from selenium.webdriver.support.ui import WebDriverWait
+    from webdriver_manager.core.utils import ChromeType
+    from webdriver_manager.firefox import GeckoDriverManager
+    from webdriver_manager.chrome import ChromeDriverManager
+    from selenium.webdriver.support import expected_conditions as EC
+    main(path=input("Enter the path to save the files: "))
     os.system("git push")
